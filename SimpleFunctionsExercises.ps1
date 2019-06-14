179ff2f921f40c6c5aea1fdbf0bff45911cfe126
AEgccAZ <51670872+AEgccAZ@users.noreply.github.com>
2019-06-14 14:06:00 -0500

    Update SimpleFunctionsExercises.ps1

--- SimpleFunctionsExercises.ps1
+++ SimpleFunctionsExercises.ps1
@@ -24,4 +24,31 @@ function Get-StartDate {
    $MyDate = Get-StartDate -Month 6 -Verbose
    $MyDate
 
+   #Example 2:
+
+   function Get-StartDate {
+    [CmdletBinding()]
+    param(
+        [Parameter(HelpMessage = "Integer month for patching" )]
+        [Int32]$Month 
+    )
+    Write-Verbose -Message "Hello World"
+    $CurrentMonth = (Get-Date -Month $Month).Month
+    switch ($(Get-Date -Month $CurrentMonth -Day 1).DayOfWeek)
+        {Sunday {$SecondWednesday = (Get-Date -Month $CurrentMonth -Day 1).AddDays(9+1)};
+        Monday {$SecondWednesday = (Get-Date -Month $CurrentMonth  -Day 1).AddDays(8+1)};
+        Tuesday {$SecondWednesday = (Get-Date -Month $CurrentMonth -Day 1).AddDays(7+1)};
+        Wednesday{$SecondWednesday = (Get-Date -Month $CurrentMonth -Day 1).AddDays(13+1)};
+        Thursday{$SecondWednesday =  (Get-Date -Month $CurrentMonth -Day 1).AddDays(12+1)};
+        Friday{$SecondWednesday = (Get-Date -Month $CurrentMonth -Day 1).AddDays(11+1)};
+        Saturday{$SecondWednesday = (Get-Date -Month $CurrentMonth -Day 1).AddDays(10+1)}}
+    return $SecondWednesday
+   }
+   $MyDate = Get-StartDate -Month 6 -Verbose
+   $MyDate
+
+#Exercise 3
+
+
+
 
